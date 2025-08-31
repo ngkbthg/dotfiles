34c74796c34a3b0ef00ffe34c62b44f441383d16
ngkbthg <ductiennguyenta@gmail.com>
2025-08-31 21:05:02 +0700

    9

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+#if status is-interactive
+    # Commands to run in interactive sessions can go here
+#end
+#set -Ux PATH $HOME/.local/bin $PATH
+set -g fish_greeting ""
+source ~/.cache/wal/colors.fish
+#wal --theme -n frieren > /dev/null 2>&1
+#fastfetch --logo-preserve-aspect-ratio --logo-width 27 --logo /home/ngkbthg/downloads/anh/sense.png 
+fastfetch -l none
+fortune -s
