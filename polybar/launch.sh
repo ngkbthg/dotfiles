9d1f164830921a35e9bc3ed57411483b0c7a20f1
ngkbthg <ductiennguyenta@gmail.com>
2025-12-29 19:46:13 +0700

    refactor: move all configs into .config directory

--- polybar/launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pkill polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-#polybar dummy &
-polybar example &
--- rofi/rofimoji.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rofimoji --selector-args="-dmenu -i -theme-str 'element-text {padding: 0px;} listview {columns: 10; lines: 10;}'" --hidden-descriptions --action copy
-
